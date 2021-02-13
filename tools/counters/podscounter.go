a03e6c46c3eb2a31f8ca7b5831111cd8116f8575
Fabrizio Soppelsa <fsoppelsa@gmail.com>
2021-02-13 21:45:10 +0100

    podscount.go

---
+++ tools/counters/podscounter.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	var namespace string
+	var kubeconfig *string
+
+	// Parse flags
+	flag.StringVar(&namespace, "n", "default", "Namespace to count on")
+	flag.Usage = func() {
+		fmt.Println("Usage")
+		fmt.Println("./podscounter -n <NAMESPACE>")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	// Load kubeconfig
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Count pods
+	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("There are %d pods in the %s namespace\n", len(pods.Items), namespace)
+}
